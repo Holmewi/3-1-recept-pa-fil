be082d05e61bb3e9fbfe0908cd65cdaee3e38447
Joakim Holmewi <jh222qr@student.lnu.se>
2014-10-29 17:18:41 +0100

    Viewing the instructions with a countdown on each

--- FiledRecipes/FiledRecipes/Views/RecipeView.cs
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -18,6 +18,22 @@ namespace FiledRecipes.Views
             Console.Clear();
             Header = recipe.Name;   // Sätter metoden Header till receptets namn
             ShowHeaderPanel();      // Skriver ut panelen där headern presenteras
+
+            // För varje ingrediens (från interfacet IIngredient) i receptets del för ingredienser, som hämtas genom IRecipeView från IRecipe
+            foreach (IIngredient ingredient in recipe.Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
+            int instructionCount = 1;
+            // För varje sträng instruktion i receptets del för instruktioner, som hämtas genom IRecipeView från IRecipe
+            foreach (string instruction in recipe.Instructions)
+            {
+
+                Console.WriteLine("<{0}>\n{1}", instructionCount, instruction);
+                instructionCount++;
+            }
+
         }
 
         // Visa alla recept
